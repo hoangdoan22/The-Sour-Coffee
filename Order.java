2601dd1b833999c5deb9e4910cf2e0096667f7e8
hoangdoan22 <130956043+hoangdoan22@users.noreply.github.com>
2023-04-17 14:01:31 +0700

    Update Order.java

--- Order.java
+++ Order.java
@@ -3,10 +3,6 @@ package homeworkOOPday3_1;
 public class Order {
 String id;
 String date;
-int quantity;
-double productPrice;
 double totalPrice;
 public Customer customer;
-public Product product;
-double total;
 }
